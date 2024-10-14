9d983d5be5a0c0c3cfd1bbd019b426b76f1b7c18
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-14 08:49:56 -0400

    Refactor directors controller to use database connection from getDb function

--- controllers/directors.js
+++ controllers/directors.js
@@ -1,10 +1,11 @@
 const Director = require('../models/director');
 const { ObjectId } = require('mongodb');
-const { getDb } = require('../database/database'); // Import the getDb function
+const { getDb } = require('../database/database');
 
 exports.getAllDirectors = async (req, res, next) => {
   try {
-    const directors = await getDb.collection('directors').find().toArray();
+    const db = getDb();
+    const directors = await db.collection('directors').find().toArray();
     res.json(directors);
   } catch (error) {
     next(error);
@@ -13,74 +14,67 @@ exports.getAllDirectors = async (req, res, next) => {
 
 exports.getDirectorById = async (req, res, next) => {
   try {
-    const director = await getDb.collection('directors').findOne({ _id: new ObjectId(req.params.id) });
+    const db = getDb();
+    const director = await db.collection('directors').findOne({ _id: new ObjectId(req.params.id) });
     if (!director) {
       return res.status(404).json({ message: 'Director not found' });
     }
     res.json(director);
   } catch (error) {
+    if (error instanceof ObjectId.TypeError) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
     next(error);
   }
 };
 
 exports.createDirector = async (req, res, next) => {
   try {
-    const director = new Director(
-      req.body.name,
-      req.body.birthYear,
-      req.body.nationality
-    );
-
-    const errors = Director.validate(director);
-    if (errors.length > 0) {
-      return res.status(400).json({ errors });
-    }
-
-    const result = await getDb.collection('directors').insertOne(director);
-    res.status(201).json({ _id: result.insertedId, ...director });
+    const db = getDb();
+    const result = await db.collection('directors').insertOne(req.body);
+    res.status(201).json({ _id: result.insertedId, ...req.body });
   } catch (error) {
+    if (error.code === 11000) {
+      return res.status(409).json({ message: 'Director already exists' });
+    }
     next(error);
   }
 };
 
 exports.updateDirector = async (req, res, next) => {
   try {
-    const director = new Director(
-      req.body.name,
-      req.body.birthYear,
-      req.body.nationality
-    );
-
-    const errors = Director.validate(director);
-    if (errors.length > 0) {
-      return res.status(400).json({ errors });
-    }
-
-    const result = await getDb.collection('directors').updateOne(
+    const db = getDb();
+    const result = await db.collection('directors').updateOne(
       { _id: new ObjectId(req.params.id) },
-      { $set: { ...director, updatedAt: new Date() } }
+      { $set: req.body }
     );
-
     if (result.matchedCount === 0) {
       return res.status(404).json({ message: 'Director not found' });
     }
-
     res.json({ message: 'Director updated successfully' });
   } catch (error) {
+    if (error instanceof ObjectId.TypeError) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
+    if (error.code === 11000) {
+      return res.status(409).json({ message: 'Director with this name already exists' });
+    }
     next(error);
   }
 };
 
 exports.deleteDirector = async (req, res, next) => {
   try {
-    const result = await getDb.collection('directors').deleteOne({ _id: new ObjectId(req.params.id) });
-
+    const db = getDb();
+    const result = await db.collection('directors').deleteOne({ _id: new ObjectId(req.params.id) });
     if (result.deletedCount === 0) {
       return res.status(404).json({ message: 'Director not found' });
     }
-
     res.json({ message: 'Director deleted successfully' });
   } catch (error) {
+    if (error instanceof ObjectId.TypeError) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
     next(error);
   }
 };
