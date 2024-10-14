7ec08a62090ae5232064c905318b654fd0738abb
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-14 08:44:00 -0400

    Refactor Swagger host URL to use the production environment

--- swagger.js
+++ swagger.js
@@ -1,38 +1,12 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-  info: {
-    title: 'Movie Collection API',
-    description: 'This is an Express API for managing a collection of movies and directors',
-  },
-  host: 'localhost:3000',
-  schemes: ['https'],
-  tags: [
-    {
-      name: 'Movies',
-      description: 'API endpoints for managing movies'
+    info: {
+        title: 'Express API for Movies and Directors',
+        description: 'This is a simple Express API for managing Movies and Directors.',
     },
-    {
-      name: 'Directors',
-      description: 'API endpoints for managing directors'
-    }
-  ],
-  definitions: {
-    Movie: {
-      title: 'Inception',
-      director: '60d725397de5c63b4a7e3b1e',  // ObjectId as string
-      releaseYear: 2010,
-      genre: ['Science Fiction', 'Action'],
-      rating: 8.8,
-      duration: 148,
-      cast: ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Ellen Page']
-    },
-    Director: {
-      name: 'Christopher Nolan',
-      birthYear: 1970,
-      nationality: 'British-American'
-    }
-  }
+    host: 'localhost:3000',
+    schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
