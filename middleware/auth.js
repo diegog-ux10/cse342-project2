4d7ac42d45b01e9c87a7a03e57cce6b931a76147
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-14 08:50:08 -0400

    Refactor authentication middleware to use database connection from getDb function

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { getDb } = require('../database/database');
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const db = getDb();
+    const user = await db.collection('users').findOne({ _id: decoded.id });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Please authenticate' });
+  }
+};
