c6e39a2f2b8312c7d8f7e607fa1358f397c56eb2
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-14 08:50:45 -0400

    Refactor movies routes to include authentication and request body validation

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const movieController = require('../controllers/movies');
-
 const router = express.Router();
+const movieController = require('../controllers/movies');
+const validate = require('../middleware/validate');
+const auth = require('../middleware/auth');
+const { movieSchema } = require('../validationSchemas');
 
+// #swagger.tags = ['Movies']
 router.get('/', movieController.getAllMovies);
+
+// #swagger.tags = ['Movies']
 router.get('/:id', movieController.getMovieById);
-router.post('/', movieController.createMovie);
-router.put('/:id', movieController.updateMovie);
-router.delete('/:id', movieController.deleteMovie);
+
+// #swagger.tags = ['Movies']
+router.post('/', auth, validate(movieSchema), movieController.createMovie);
+
+// #swagger.tags = ['Movies']
+router.put('/:id', auth, validate(movieSchema), movieController.updateMovie);
+
+// #swagger.tags = ['Movies']
+router.delete('/:id', auth, movieController.deleteMovie);
 
 module.exports = router;
