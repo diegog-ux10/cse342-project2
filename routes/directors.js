c3b7b630516e6153bd0354c56db6eda0b002ace0
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-14 08:50:37 -0400

    Refactor directors routes to include authentication and request body validation

--- routes/directors.js
+++ routes/directors.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const directorController = require('../controllers/directors');
-
 const router = express.Router();
+const directorController = require('../controllers/directors');
+const validate = require('../middleware/validate');
+const auth = require('../middleware/auth');
+const { directorSchema } = require('../validationSchemas');
 
+// #swagger.tags = ['Directors']
 router.get('/', directorController.getAllDirectors);
+
+// #swagger.tags = ['Directors']
 router.get('/:id', directorController.getDirectorById);
-router.post('/', directorController.createDirector);
-router.put('/:id', directorController.updateDirector);
-router.delete('/:id', directorController.deleteDirector);
+
+// #swagger.tags = ['Directors']
+router.post('/', auth, validate(directorSchema), directorController.createDirector);
+
+// #swagger.tags = ['Directors']
+router.put('/:id', auth, validate(directorSchema), directorController.updateDirector);
+
+// #swagger.tags = ['Directors']
+router.delete('/:id', auth, directorController.deleteDirector);
 
 module.exports = router;
