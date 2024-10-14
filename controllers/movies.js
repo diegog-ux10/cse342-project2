c678e01e98154e365dcd54333ff55f7702931b24
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-14 08:50:03 -0400

    Refactor movies controller to use database connection from getDb function

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,9 +1,11 @@
 const Movie = require('../models/movie');
 const { ObjectId } = require('mongodb');
+const { getDb } = require('../database/database');
 
 exports.getAllMovies = async (req, res, next) => {
   try {
-    const movies = await req.app.locals.db.collection('movies').find().toArray();
+    const db = getDb();
+    const movies = await db.collection('movies').find().toArray();
     res.json(movies);
   } catch (error) {
     next(error);
@@ -12,35 +14,25 @@ exports.getAllMovies = async (req, res, next) => {
 
 exports.getMovieById = async (req, res, next) => {
   try {
-    const movie = await req.app.locals.db.collection('movies').findOne({ _id: new ObjectId(req.params.id) });
+    const db = getDb();
+    const movie = await db.collection('movies').findOne({ _id: new ObjectId(req.params.id) });
     if (!movie) {
       return res.status(404).json({ message: 'Movie not found' });
     }
     res.json(movie);
   } catch (error) {
+    if (error instanceof ObjectId.TypeError) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
     next(error);
   }
 };
 
 exports.createMovie = async (req, res, next) => {
   try {
-    const movie = new Movie(
-      req.body.title,
-      new ObjectId(req.body.director),
-      req.body.releaseYear,
-      req.body.genre,
-      req.body.rating,
-      req.body.duration,
-      req.body.cast
-    );
-
-    const errors = Movie.validate(movie);
-    if (errors.length > 0) {
-      return res.status(400).json({ errors });
-    }
-
-    const result = await req.app.locals.db.collection('movies').insertOne(movie);
-    res.status(201).json({ _id: result.insertedId, ...movie });
+    const db = getDb();
+    const result = await db.collection('movies').insertOne(req.body);
+    res.status(201).json({ _id: result.insertedId, ...req.body });
   } catch (error) {
     next(error);
   }
@@ -48,46 +40,35 @@ exports.createMovie = async (req, res, next) => {
 
 exports.updateMovie = async (req, res, next) => {
   try {
-    const movie = new Movie(
-      req.body.title,
-      new ObjectId(req.body.director),
-      req.body.releaseYear,
-      req.body.genre,
-      req.body.rating,
-      req.body.duration,
-      req.body.cast
-    );
-
-    const errors = Movie.validate(movie);
-    if (errors.length > 0) {
-      return res.status(400).json({ errors });
-    }
-
-    const result = await req.app.locals.db.collection('movies').updateOne(
+    const db = getDb();
+    const result = await db.collection('movies').updateOne(
       { _id: new ObjectId(req.params.id) },
-      { $set: { ...movie, updatedAt: new Date() } }
+      { $set: req.body }
     );
-
     if (result.matchedCount === 0) {
       return res.status(404).json({ message: 'Movie not found' });
     }
-
     res.json({ message: 'Movie updated successfully' });
   } catch (error) {
+    if (error instanceof ObjectId.TypeError) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
     next(error);
   }
 };
 
 exports.deleteMovie = async (req, res, next) => {
   try {
-    const result = await req.app.locals.db.collection('movies').deleteOne({ _id: new ObjectId(req.params.id) });
-
+    const db = getDb();
+    const result = await db.collection('movies').deleteOne({ _id: new ObjectId(req.params.id) });
     if (result.deletedCount === 0) {
       return res.status(404).json({ message: 'Movie not found' });
     }
-
     res.json({ message: 'Movie deleted successfully' });
   } catch (error) {
+    if (error instanceof ObjectId.TypeError) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
     next(error);
   }
 };
