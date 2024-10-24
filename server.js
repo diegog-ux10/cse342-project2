b659437f4a71b91ee073dda6ed968a696cd6e1bb
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-24 09:37:06 -0400

    Refactor server.js to use httpsOnly flag for session cookies

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
+    httpsOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
